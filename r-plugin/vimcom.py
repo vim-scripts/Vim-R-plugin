e2afb6108f62b2c5d26ac07f35c0d13828642009
Jakson Aquino <jalvesaq@gmail.com>
2013-11-01 11:44:21 -0300

    Require vimcom.plus (or vimcom) 0.9-91.

--- r-plugin/vimcom.py
+++ r-plugin/vimcom.py
@@ -50,8 +50,8 @@ def DiscoverVimComPort():
     else:
         vim.command("let g:rplugin_vimcomport = " + str(VimComPort))
         PortWarn = False
-        if repl.find("0.9-9") != 0:
-            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom.plus (or vimcom) 0.9-9.')")
+        if repl.find("0.9-91") != 0:
+            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom.plus (or vimcom) 0.9-91.')")
             vim.command("sleep 1")
     return(VimComPort)
 
