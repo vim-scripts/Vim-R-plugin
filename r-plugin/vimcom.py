f30cf21f9e066c0d3267a8f5d3c3e4289aa90737
Jakson Aquino <jalvesaq@gmail.com>
2014-07-02 14:18:15 -0300

    Version 1.0.

--- r-plugin/vimcom.py
+++ r-plugin/vimcom.py
@@ -52,8 +52,8 @@ def DiscoverVimComPort():
     else:
         vim.command("let g:rplugin_vimcomport = " + str(VimComPort))
         PortWarn = False
-        if repl.find("1.0-0_a4") != 0:
-            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom 1.0-0_a4.')")
+        if repl.find("1.0-0") != 0:
+            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom 1.0-0.')")
             vim.command("sleep 1")
     return(VimComPort)
 
