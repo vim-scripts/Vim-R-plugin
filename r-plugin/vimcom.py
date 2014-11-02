f5e65cd6b1ea24f1228c9be94d6c78140ef99c74
Jakson Alves de Aquino <jalvesaq@gmail.com>
2014-11-01 22:00:23 -0400

    Require vimcom 1.0-5.

--- r-plugin/vimcom.py
+++ r-plugin/vimcom.py
@@ -59,8 +59,8 @@ def DiscoverVimComPort():
     else:
         vim.command("let g:rplugin_vimcomport = " + str(VimComPort))
         PortWarn = 0
-        if repl.find("1.0-4") != 0:
-            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom 1.0-4.')")
+        if repl.find("1.0-5") != 0:
+            vim.command("call RWarningMsg('This version of Vim-R-plugin requires vimcom 1.0-5.')")
             vim.command("sleep 1")
     return(VimComPort)
 
